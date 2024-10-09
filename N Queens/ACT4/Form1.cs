4a4f69dfcda88699227d0a223e8ac36e47f7b5fd
Ellydhore <110271100+Ellydhore@users.noreply.github.com>
2024-10-09 11:00:39 +0800

    Add files via upload

---
+++ N Queens/ACT4/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ACT4
+{
+    public partial class Form1 : Form
+    {
+        int side;
+        int n = 6;
+        SixState startState;
+        SixState currentState;
+        int moveCounter;
+
+        int[,] hTable;
+        ArrayList bMoves;
+        Object chosenMove;
+        double temperature = 100; // Starting temperature
+        double coolingRate = 0.95; // Cooling rate
+
+        public Form1()
+        {
+            InitializeComponent();
+            side = pictureBox1.Width / n;
+
+            startState = randomSixState();
+            currentState = new SixState(startState);
+
+            updateUI();
+            label1.Text = "Attacking pairs: " + getAttackingPairs(startState);
+        }
+
+        private void updateUI()
+        {
+            pictureBox2.Refresh();
+            label3.Text = "Attacking pairs: " + getAttackingPairs(currentState);
+            label4.Text = "Moves: " + moveCounter;
+            hTable = getHeuristicTableForPossibleMoves(currentState);
+            bMoves = getBestMoves(hTable);
+
+            listBox1.Items.Clear();
+            foreach (Point move in bMoves)
+            {
+                listBox1.Items.Add(move);
+            }
+
+            if (bMoves.Count > 0)
+                chosenMove = chooseMove(bMoves);
+            label2.Text = "Chosen move: " + chosenMove;
+        }
+
+        private void pictureBox2_Paint(object sender, PaintEventArgs e)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if ((i + j) % 2 == 0)
+                    {
+                        e.Graphics.FillRectangle(Brushes.Black, i * side, j * side, side, side);
+                    }
+                    if (j == currentState.Y[i])
+                        e.Graphics.FillEllipse(Brushes.Fuchsia, i * side, j * side, side, side);
+                }
+            }
+        }
+
+        private SixState randomSixState()
+        {
+            Random r = new Random();
+            SixState random = new SixState(r.Next(n),
+                                            r.Next(n),
+                                            r.Next(n),
+                                            r.Next(n),
+                                            r.Next(n),
+                                            r.Next(n));
+            return random;
+        }
+
+        private int getAttackingPairs(SixState f)
+        {
+            int attackers = 0;
+            for (int rf = 0; rf < n; rf++)
+            {
+                for (int tar = rf + 1; tar < n; tar++)
+                {
+                    if (f.Y[rf] == f.Y[tar])
+                        attackers++;
+                    if (f.Y[tar] == f.Y[rf] + tar - rf)
+                        attackers++;
+                    if (f.Y[rf] == f.Y[tar] + tar - rf)
+                        attackers++;
+                }
+            }
+            return attackers;
+        }
+
+        private int[,] getHeuristicTableForPossibleMoves(SixState thisState)
+        {
+            int[,] hStates = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    SixState possible = new SixState(thisState);
+                    possible.Y[i] = j;
+                    hStates[i, j] = getAttackingPairs(possible);
+                }
+            }
+            return hStates;
+        }
+
+        private ArrayList getBestMoves(int[,] heuristicTable)
+        {
+            ArrayList bestMoves = new ArrayList();
+            int bestHeuristicValue = heuristicTable[0, 0];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (bestHeuristicValue >= heuristicTable[i, j])
+                    {
+                        bestHeuristicValue = heuristicTable[i, j];
+                        if (currentState.Y[i] != j)
+                            bestMoves.Add(new Point(i, j));
+                    }
+                }
+            }
+            label5.Text = "Possible Moves (H=" + bestHeuristicValue + ")";
+            return bestMoves;
+        }
+
+        private Object chooseMove(ArrayList possibleMoves)
+        {
+            Random r = new Random();
+            int randomMove = r.Next(possibleMoves.Count);
+            return possibleMoves[randomMove];
+        }
+
+        private void executeMove(Point move)
+        {
+            SixState nextState = new SixState(currentState);
+            nextState.Y[move.X] = move.Y;
+
+            int currentAttacks = getAttackingPairs(currentState);
+            int nextAttacks = getAttackingPairs(nextState);
+
+            // Accept the move based on temperature and energy difference
+            if (nextAttacks < currentAttacks || (Math.Exp((currentAttacks - nextAttacks) / temperature) > new Random().NextDouble()))
+            {
+                currentState.Y[move.X] = move.Y;
+                moveCounter++;
+            }
+
+            // Cool down the temperature
+            temperature *= coolingRate;
+
+            chosenMove = null;
+            updateUI();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (getAttackingPairs(currentState) > 0)
+                executeMove((Point)chosenMove);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            startState = randomSixState();
+            currentState = new SixState(startState);
+            moveCounter = 0;
+            temperature = 100; // Reset temperature
+            updateUI();
+            pictureBox1.Refresh();
+            label1.Text = "Attacking pairs: " + getAttackingPairs(startState);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            while (getAttackingPairs(currentState) > 0)
+            {
+                executeMove((Point)chosenMove);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e) { }
+    }
+}
